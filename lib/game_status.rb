8f3cd3ac0be426c5438e7e3337a9fe0e791136b0
Jordan Ginor <jordan.ginor@gmail.com>
2018-12-04 22:07:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,40 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8]
+  ]
+  
+def won?(board)
+win_index1 = WIN_COMBINATIONS[0]
+win_index2 = WIN_COMBINATIONS[1]
+win_index3 = WIN_COMBINATIONS[2]
+win_index4 = WIN_COMBINATIONS[3]
+win_index5 = WIN_COMBINATIONS[4]
+win_index6 = WIN_COMBINATIONS[5]
+win_index7 = WIN_COMBINATIONS[6]
+win_index8 = WIN_COMBINATIONS[7]
+each.WIN_COMBINATIONS do|win_index|  
+  if board[win_index[0]] == "X" &&
+     board[win_index[1]] == "X" &&
+     board[win_index[2]] == "X"
+    
+    "true" 
+  else
+    false 
+  end
+end 
+
+# all_odd = [1,2,3].all? do |number|
+#  number.odd? 
+# end 
+
+# [1,2,100].any?{|i| i > 99} 
