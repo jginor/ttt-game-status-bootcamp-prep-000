c28f014e142b7b91c3a82086427cf431ed0809f0
Jordan Ginor <jordan.ginor@gmail.com>
2018-12-04 23:41:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,42 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8]
+  ]
+  
+def won?(board)
+  WIN_COMBINATIONS.each do|win_index|
+    win_index.each do |index|
+      if board[index] == "X" || board[index] =="O"
+        WIN_COMBINATIONS[win_index]
+      else
+        false
+      end
+    end
+  end
+end
+
+
+board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+won?(board)
+
+#nested_students.each do |student_array|
+#  student_array.each do |student_detail|
+#    puts student_detail
+#  end
+#end
+
+# all_odd = [1,2,3].all? do |number|
+#  number.odd? 
+# end 
+
+# [1,2,100].any?{|i| i > 99} 
